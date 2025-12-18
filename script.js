220c563eba8e3ffe64c8c25de4c009accc5088d7
Saurabh Yadav <104919948+SaurabhYadav100@users.noreply.github.com>
2025-12-19 01:08:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Contact form
+document.getElementById("contactForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Thank you! We’ve received your message and will reply soon.");
+  e.target.reset();
+});
+// Navigation Bar
+let lastScrollTop = 0;
+    const navbar = document.querySelector("header");
+
+
+    window.addEventListener("scroll", function() {
+      const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScroll > lastScrollTop) {
+        // Scrolling down → hide navbar
+        navbar.style.top = "-90px";
+      } else {
+        // Scrolling up → show navbar
+        navbar.style.top = "0";
+      }
+
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    });
+
